4e1600daf3050e43045969ff4da708b905084e39
alexey.krylov <turn2river@gmail.com>
2023-04-23 11:58:32 +0300

    refactor(basic-3.4): remove tag selectors

--- app.js
+++ app.js
@@ -32,20 +32,24 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className="task-list-item"
+
     label.innerText=taskString;
-    label.className='task';
+    label.className="label-task";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="input-checkbox"
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="input-task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="button edit";
 
-    deleteButton.className="delete";
-    deleteButtonImg.src='./remove.svg';
+    deleteButton.className="button delete";
+    deleteButtonImg.src="./remove.svg";
     deleteButton.appendChild(deleteButtonImg);
+    deleteButtonImg.className="delete-image"
 
 
     //and appending.
@@ -82,7 +86,7 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector("input[type=text]");
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("edit-mode");
